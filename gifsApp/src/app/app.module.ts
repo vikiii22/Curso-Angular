9b58f8e983afc18ac3315fe37545dfcb2469ffe0
vikiii22 <joseviki10@gmail.com>
2022-04-13 12:03:26 +0200

    app gifs obteniendo desde servidor

--- gifsApp/src/app/app.module.ts
+++ gifsApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,8 +12,9 @@ import { GifsModule } from './gifs/gifs.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
-    GifsModule
+    GifsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,21 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+  
+  private _historial: string[] = [];
+  private apiKey = '1Es7hV1yEJjmV6WscxhNWomjSox6MQkb';
+  private urlEjemplo = 'https://api.giphy.com/v1/gifs/search?api_key=1Es7hV1yEJjmV6WscxhNWomjSox6MQkb&q=dragon%20ball'
 
-  // constructor() { }
-
-  private _historial:string[]=[];
-
-  get historial(){
+  get historial() {
     return [...this._historial];
   }
 
-  buscarGif(buscado:string){
+  constructor(private http:HttpClient){}
+
+  buscarGif(buscado: string) {
+    buscado = buscado.toLocaleLowerCase();
+    if (buscado.trim().length == 0) {
+      return;
+    }
+    if (this._historial.includes(buscado)) {
+      return;
+    }
+    this._historial = this._historial.splice(0, 10);
     this._historial.unshift(buscado);
 
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=1Es7hV1yEJjmV6WscxhNWomjSox6MQkb&q=' + buscado)
+      .subscribe( resp => {
+        console.log(resp);
+      });
   }
-
 }

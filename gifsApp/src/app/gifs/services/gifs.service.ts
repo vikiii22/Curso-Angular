a34c77deb4705df99085ed48c9f7d195819eabe9
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2022-04-19 10:35:08 +0200

    app de gifs acabada

--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
@@ -10,6 +10,7 @@ export class GifsService {
   private _historial: string[] = [];
   private apiKey = '1Es7hV1yEJjmV6WscxhNWomjSox6MQkb';
   public resultado: Gif[] = [];
+  private servicioUrl='https://api.giphy.com/v1/gifs';
 
   get historial() {
     return [...this._historial];
@@ -36,7 +37,11 @@ export class GifsService {
 
     localStorage.setItem('historial', JSON.stringify(this._historial));
 
-    this.http.get<SearchGifsResponse>('https://api.giphy.com/v1/gifs/search?api_key=1Es7hV1yEJjmV6WscxhNWomjSox6MQkb&q=' + buscado)
+    const params=new HttpParams()
+      .set('apiKey', this.apiKey)
+      .set('q', buscado);
+
+    this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params })
       .subscribe((resp) => {
         console.log(resp);
         this.resultado = resp.data;

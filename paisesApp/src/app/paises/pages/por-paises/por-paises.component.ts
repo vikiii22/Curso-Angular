7c1649d7bf116eec58b46f0d07d6377a899dde2e
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2022-04-20 18:23:25 +0200

    paises

--- paisesApp/src/app/paises/pages/por-paises/por-paises.component.ts
+++ paisesApp/src/app/paises/pages/por-paises/por-paises.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-paises',
@@ -6,11 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisesComponent implements OnInit {
+export class PorPaisesComponent {
+  termino: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  buscar() {
+    if (this.termino == "") {
+      return;
+    } else {
+      console.log(this.termino);
+    }
   }
-
 }
--- paisesApp/src/app/paises/paises.module.ts
+++ paisesApp/src/app/paises/paises.module.ts
@@ -4,6 +4,7 @@ import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisesComponent } from './pages/por-paises/por-paises.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -21,7 +22,8 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     VerPaisComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class PaisesModule { }

001dc33524d88766bcd594e43b89c91e112e987a
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2022-04-26 09:10:19 +0200

    app pais acabada

--- paisesApp/src/app/paises/pages/ver-pais/ver-pais.component.ts
+++ paisesApp/src/app/paises/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
 import { PaisService } from '../../services/pais.service';
+import { InterfacePaises } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -10,16 +12,28 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!:InterfacePaises;
+
   constructor(private activatedRoute: ActivatedRoute, private paisService: PaisService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params
-      .subscribe(({ codigoPais }) => {
-        this.paisService.getPaisPorCodigo(codigoPais)
-          .subscribe(pais=>{
-            console.log(pais);
-          })
+      .pipe(
+        switchMap(({ codigoPais }) => this.paisService.getPaisPorCodigo(codigoPais)),
+        tap(console.log)
+      )
+      .subscribe(resp => {
+        this.pais=resp;
       })
+
+
+    // this.activatedRoute.params
+    //   .subscribe(({ codigoPais }) => {
+    //     this.paisService.getPaisPorCodigo(codigoPais)
+    //       .subscribe(pais=>{
+    //         console.log(pais);
+    //       })
+    //   })
   }
 
 }

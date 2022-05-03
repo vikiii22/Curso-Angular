d0f2dec7b4bbb57160ef8b62d1b0e16c89baa793
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2022-05-03 17:31:31 +0200

    json server

--- lazyLoadAPP/src/app/heroes/pages/heroes-home/heroes-home.component.ts
+++ lazyLoadAPP/src/app/heroes/pages/heroes-home/heroes-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes-home',
@@ -12,9 +13,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private heroesService:HeroesService) { }
 
   ngOnInit(): void {
+    this.heroesService.getHeroes().subscribe( resp=> console.log( resp ))
   }
 
 }

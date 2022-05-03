606c6d527d5e891285fadd911a179853b0051656
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2022-05-03 17:31:47 +0200

    json server

--- lazyLoadAPP/src/app/app.module.ts
+++ lazyLoadAPP/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,7 +18,8 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    AuthModule
+    AuthModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

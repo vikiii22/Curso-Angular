de85706221427856bd28fd3ad914c4179d80b80c
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2022-05-12 14:58:34 +0200

    template

--- formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
+++ formulariosApp/src/app/template/dinamicos/dinamicos.component.ts
@@ -1,20 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
+
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent{
+
+  persona:Persona={
+    nombre:'Jose Luis',
+    favoritos:[
+      {
+        id: 1,
+        nombre:`Assassyn's creed`
+      },
+      {
+        id: 2,
+        nombre: 'Far cry'
+      }
+    ]
+  }
+
+  nuevoJuego:string='';
 
   constructor() { }
 
-  ngOnInit(): void {
+  guardar() {
+
   }
 
-  guardar(){
-    
+  agregar(){
+    const nuevo:Favorito={id: this.persona.favoritos.length + 1, nombre:this.nuevoJuego};
+    this.persona.favoritos.push(nuevo);
+    this.nuevoJuego='';
+  }
+
+  eliminar( id:number ){
+    this.persona.favoritos.splice(id, 1);
   }
 
 }

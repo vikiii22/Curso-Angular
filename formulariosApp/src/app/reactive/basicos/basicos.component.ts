769bdb7406b4650faa48b4cee4e906abefee0f18
Jose Antonio Sánchez López <80800807+vikiii22@users.noreply.github.com>
2022-05-12 17:49:59 +0200

    reactivo

--- formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
+  // miFormulario:FormGroup=new FormGroup({
+  //   nombre:new FormControl('RTX 4080 TI'),
+  //   precio:new FormControl(1500),
+  //   existencias: new FormControl(100)
+  // });
 
-  ngOnInit(): void {
+  constructor( private formBuilder:FormBuilder ) { }
+
+  miFormulario:FormGroup=this.formBuilder.group({
+    nombre:[ , [Validators.required, Validators.minLength(3)]],
+    precio:[ , [Validators.required, Validators.min(0)]],
+    existencias:[ , [Validators.required, Validators.min(0)]]
+  });
+
+  campoValido( campo:string ):boolean | null{
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
   }
-
 }
